3d8c720ff199ded9acfeed4ca4d2fa5eecadfd3b
dheeraj <dheerajojha500@gmail.com>
2024-05-04 19:50:11 +0530

    h

--- MERN/Frontend/src/Components/Footer.jsx
+++ MERN/Frontend/src/Components/Footer.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <FooterWrapper>
+      <div className="row">
+        <div className="col"><ul>
+          <li><h3>Logo.</h3></li>
+          <li><p>The best look anytime, anywhere.</p></li>
+        </ul></div>
+        <div className="col">
+          <ul>
+            <li><h4>For Her</h4></li>
+            <li><Link to="/">Jeens</Link></li>
+            <li><Link to="/">Jeens</Link></li>
+            <li><Link to="/">Jeens</Link></li>
+            <li><Link to="/">Jeens</Link></li>
+            <li><Link to="/">Jeens</Link></li>
+          </ul>
+        </div>
+        <div className="col"> <ul>
+          <li><h4>For Him</h4></li>
+          <li><Link to="/">Jeens</Link></li>
+          <li><Link to="/">Jeens</Link></li>
+          <li><Link to="/">Jeens</Link></li>
+          <li><Link to="/">Jeens</Link></li>
+          <li><Link to="/">Jeens</Link></li>
+        </ul></div>
+        <div className="col"> <ul>
+          <li><h4>Subscribe</h4></li>
+          <li><input type="text" />
+            <Link to="" className='cta'>Subscibe</Link>
+          </li>
+        </ul></div>
+      </div>
+
+      <div className="row flexBetween">
+        <div className="left">
+          <p>Copyright © 2024 Brandstore. Powered by Brandstore.</p>
+        </div>
+        <div className="right"><ul className='flexRow'>
+          <li><Link to="/"><FaFacebook /></Link></li>
+          <li><Link to="/"><FaYoutube /></Link></li>
+          <li><Link to="/"><FaTwitter /></Link></li>
+          <li><Link to="/"><FaWhatsapp /></Link></li>
+          <li><Link to="/"><FaInstagram /></Link></li>
+        </ul></div>
+      </div>
+    </FooterWrapper>
   )
 }
+
+const FooterWrapper = styled.footer`
+.row{
+  ul{
+    li{
+      line-height:2rem;
+    }
+  }
+  &:nth-child(1){
+    padding-bottom:2rem;
+    display:flex;
+    justify-content:space-between;
+    flex-wrap:wrap;
+  }
+  &:nth-child(2){
+    border-top:1px solid var(--grayColor);
+    padding-top:2rem;
+    flex-wrap:wrap;
+    text-align:center;
+    a{
+      color:var(--blackColor);
+    }
+  }
+}
+
+
+`
+
+
 
 export default Footer
--- MERN/Frontend/src/Components/Header.jsx
+++ MERN/Frontend/src/Components/Header.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import {AiOutlineShoppingCart,AiOutlineUser} from "react-icons/ai";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
+  const[cart,setCart]=useState(0)
+  const headerLeftData = [{
+    title: 'Accessories',
+    path: 'accessories',
+  },
+  {
+    title: 'Mens',
+    path: 'mens',
+  },
+  {
+    title: 'Women',
+    path: 'women',
+  },
+  {
+    title: 'All',
+    path: 'all',
+  },
+  ]
+
+  const headerRightData = [{
+    title: 'About',
+    path: 'about',
+  },
+  {
+    title: 'Contact',
+    path: 'contact',
+  },
+  ]
   return (
-    <div>Header</div>
+    <HeaderWrapper>
+      <div className="left flexRow">
+        <div className="logo">
+          <Link to="/">LOGO</Link>
+        </div>
+        <ul className='flexRow'>
+          {headerLeftData.map((item, index) => {
+            return <li key={index}>
+              <Link to={item.path}>{item.title}</Link>
+            </li>
+          })}
+        </ul>
+      </div>
+      <div className="right flexRow">
+       <ul className='flexRow'>
+        {headerRightData.map((item,index)=>{
+          return <li key={index}>
+            <Link to={item.path}>{item.title}</Link>
+          </li>
+        })}
+           <li className='cart-container'>
+            <Link to=""><AiOutlineShoppingCart/></Link>
+            <span>{cart}</span>
+          </li>
+          <li >
+            <Link to=""><AiOutlineUser/></Link>
+          </li>
+          <li className='bar-container'><FaBars/></li>
+       </ul>
+      </div>
+    </HeaderWrapper>
   )
 }
 
+const HeaderWrapper = styled.header`
+background-color:rgba(36, 124, 179, 0.618);
+padding:40px 3%;
+display:flex;
+justify-content:space-between;
+position:absolute;
+width:100%;
+.left{
+  .logo{
+    a{
+      color:var(--whiteColor);
+      font-size:28px;
+      font-weight:'600';}
+  }
+}
+ul{
+  a{
+    color:var(--whiteColor);
+    font-weight:'600';
+    text-transform:uppercase;
+  }
+ .cart-container{
+  position:relative;
+  span{
+    position:absolute;
+    right:-15px;
+    top:-15px;
+    color:var(--whiteColor);
+    background-color:red;
+    width:20px;
+    height:20px;
+    border-radius:50%;
+    display:grid;
+    place-content:center;
+    font-size:14px;
+  }
+ }
+ .bar-container{
+  background-color:var(--blackColor);
+  color:var(--whiteColor);
+  padding:.5rem;
+  /* display:none; */
+ }
+}
+
+`
 export default Header
