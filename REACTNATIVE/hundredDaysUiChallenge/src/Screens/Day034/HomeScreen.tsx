948556ea8e9064b9efb234b77c06cb9e742a400a
dheeraj <dheerajojha500@gmail.com>
2024-07-12 22:00:23 +0530

    day34.

--- REACTNATIVE/hundredDaysUiChallenge/src/Screens/Day034/HomeScreen.tsx
+++ REACTNATIVE/hundredDaysUiChallenge/src/Screens/Day034/HomeScreen.tsx
@@ -1,10 +1,12 @@
-import { ImageBackground, StyleSheet, Text, View, Dimensions, StatusBar, TextInput, TouchableOpacity, Image, FlatList, Pressable } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View, Dimensions, StatusBar, TextInput, TouchableOpacity, Image, FlatList, Pressable, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AntIcon from "react-native-vector-icons/AntDesign"
+import EvilIcon from "react-native-vector-icons/EvilIcons"
 import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons"
 import GlobalStyles from '../../Constants/GlobalStyles'
 import Carousel from 'react-native-reanimated-carousel'
+import LottieView from 'lottie-react-native'
 const { height, width } = Dimensions.get('window')
 const HomeScreen = () => {
   interface carouselType {
@@ -79,20 +81,47 @@ const HomeScreen = () => {
     image: require('../../Assets/Images/Day034/more.png')
   },
   ]
+
+  const trendingData = [{
+    name: "Hotel Liverpool",
+    description: "Hotel liverpool with high end feature plus additional services available 24*7",
+    price: 200,
+    location: "Liverpool Mg 28-23",
+    image: require('../../Assets/Images/Day034/t1.jpg'),
+    rating: 4.4,
+    relatedImage: [require('../../Assets/Images/Day034/t1.jpg'), require('../../Assets/Images/Day034/t2.jpg'), require('../../Assets/Images/Day034/t3.jpg')]
+  },
+  {
+    name: "Hotel Manchester",
+    description: "Hotel Manchester with high end feature plus additional services available 24*7",
+    price: 240,
+    location: "Manchester Mg 28-23",
+    image: require('../../Assets/Images/Day034/t2.jpg'),
+    rating: 4.2,
+    relatedImage: [require('../../Assets/Images/Day034/t1.jpg'), require('../../Assets/Images/Day034/t2.jpg'), require('../../Assets/Images/Day034/t3.jpg')]
+  },
+  {
+    name: "Hotel City",
+    description: "Hotel City with high end feature plus additional services available 24*7",
+    price: 280,
+    location: "City Mg 28-23",
+    image: require('../../Assets/Images/Day034/t3.jpg'),
+    rating: 4.4,
+    relatedImage: [require('../../Assets/Images/Day034/t1.jpg'), require('../../Assets/Images/Day034/t2.jpg'), require('../../Assets/Images/Day034/t3.jpg')]
+  },
+  ]
   return (
     <>
       <StatusBar backgroundColor={'transparent'} translucent />
-      <View style={styles.homeWrapper}>
-        <ImageBackground source={require('../../Assets/Images/Day034/bg1.jpg')} style={{ height: height / 3, width: width, }}>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.homeWrapper}>
+        <ImageBackground source={require('../../Assets/Images/Day034/bg1.jpg')} style={{ height: height / 2.8, width: width, }}>
           <SafeAreaView style={{ padding: 10, gap: 15 }}>
             {/* header start */}
             <View style={GlobalStyles.flexBetween}>
-              <View>
-                <Text>admin</Text>
-              </View>
-
-              <View>
-                <Text>Canavas</Text>
+              <LottieView source={require('../../Assets/Animations/user.json')} loop={true} autoPlay={true} style={{ width: 60, height: 60, }} />
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <EvilIcon name='location' size={24} color="#000" />
+                <Text style={styles.h3}>Canavas</Text>
               </View>
 
               <View style={styles.iconContainer}>
@@ -114,7 +143,7 @@ const HomeScreen = () => {
         </ImageBackground>
 
         {/* second part start */}
-        <View style={{ padding: 10,gap:15 }}>
+        <View style={{ padding: 10, gap: 15 }}>
 
           {/* carousel start */}
           <View style={{ alignItems: 'center' }}>
@@ -147,7 +176,7 @@ const HomeScreen = () => {
           </View>
 
           {/* category start */}
-          <FlatList contentContainerStyle={{ gap: 20, marginTop: 220 }} numColumns={4} data={categoryData} renderItem={({ item, index }) => (
+          <FlatList contentContainerStyle={{ gap: 20, }} numColumns={4} data={categoryData} renderItem={({ item, index }) => (
             <Pressable key={index} style={{ padding: 10, marginRight: 20, alignItems: 'center', gap: 15 }}>
               <Image source={item.image} style={{ width: 40, height: 40 }} />
               <Text>{item.name}</Text>
@@ -159,8 +188,25 @@ const HomeScreen = () => {
             <Text style={styles.h2}>Trending</Text>
             <Text style={styles.small}>View All</Text>
           </View>
+
+          <FlatList contentContainerStyle={{ gap: 20 }} showsHorizontalScrollIndicator={false} horizontal data={trendingData} renderItem={({ item }) => (
+            <TouchableOpacity style={{ backgroundColor: "#fff", }}>
+              <Image source={item.image} style={{ height: height / 4, width: width * 0.95, borderRadius: 10 }} />
+              <View style={{ padding: 10, gap: 5 }}>
+                <Text style={styles.h3}>{item.name}</Text>
+                <Text><EvilIcon name="location" size={20} />{item.location}</Text>
+                <View style={GlobalStyles.flexBetween}>
+                  <View style={GlobalStyles.flexRow}>
+                    <Text style={styles.h3}>${item.price}</Text>
+                    <Text>/Night</Text>
+                  </View>
+                  <Text>⭐{item.rating}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )} />
         </View>
-      </View>
+      </ScrollView>
     </>
   )
 }
@@ -169,8 +215,9 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
   h1: { color: '#fff', fontSize: 30 },
-  h2: { fontWeight:'600', fontSize: 20,color:'#000' },
-  small: { fontWeight:'600', fontSize: 14,color:'red' },
+  h2: { fontWeight: '600', fontSize: 20, color: '#000' },
+  h3: { fontWeight: '500', fontSize: 18, color: '#000' },
+  small: { fontWeight: '600', fontSize: 14, color: 'red' },
   homeWrapper: { gap: 15, },
   searchContainer: {
     backgroundColor: "#fff", flexDirection: 'row', alignItems: 'center',
