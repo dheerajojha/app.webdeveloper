b88d25c963149a7834b4f67cf78ca746a1a36585
dheeraj <dheerajojha500@gmail.com>
2024-07-30 19:34:45 +0530

    coffeew

---
+++ NEXTJS/coffee/src/Components/index.js
@@ -0,0 +1,9 @@
+import Header from "./Common/Header/Header.jsx"
+import Footer from "./Common/Footer/Footer.jsx"
+import Hero from "./Hero/Hero"
+import About from "./About/About.jsx"
+import Exploremore from "./Exploremore/Exploremore.jsx"
+import Testimonial from "./Testimonial/Testimonial.jsx"
+import Newsletter from "./Newsletter/Newsletter.jsx"
+
+export {Header,Footer,Hero,About,Exploremore,Testimonial,Newsletter}
---
+++ NEXTJS/coffee/src/app/page.js
@@ -0,0 +1,16 @@
+import { About, Exploremore, Hero, Newsletter, Testimonial } from '@/Components'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Hero/>
+    <About/>
+    <Exploremore/>
+    <Testimonial/>
+   <Newsletter/>
+    </>
+  )
+}
+
+export default page
