e71549f9976fa85c1d716641fac298c51ea8e5ce
dheeraj <dheerajojha500@gmail.com>
2025-01-12 14:43:49 +0530

    h

---
+++ REACTJS/travel/src/Components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className="banner-wrapper">
+      <article>
+        <h1 className='text-white'>Plan The Perfect Winter Trip</h1>
+        <p>Easily Plan Your Idea and Travel the world with ease</p>
+      </article>
+    </section>
+  )
+}
+
+export default Banner
---
+++ REACTJS/travel/src/Components/index.js
@@ -0,0 +1,5 @@
+import Header from './Common/Header'
+import Footer from './Common/Footer'
+import Banner from './Banner'
+
+export {Header,Footer,Banner}
